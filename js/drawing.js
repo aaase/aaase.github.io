e8eacab581df1d185f3330807350cec96152cd08
Ivan Melnikov <ivan.melnikov@deposit-solutions.com>
2019-04-02 23:02:34 +0200

    Initial

---
+++ js/drawing.js
@@ -0,0 +1,44 @@
+function resizeCanvasAndResults(dimensions, canvas, results) {
+  const { width, height } = dimensions instanceof HTMLVideoElement
+    ? faceapi.getMediaDimensions(dimensions)
+    : dimensions
+  canvas.width = width
+  canvas.height = height
+
+  // resize detections (and landmarks) in case displayed image is smaller than
+  // original size
+  return faceapi.resizeResults(results, { width, height })
+}
+
+function drawDetections(dimensions, canvas, detections) {
+  const resizedDetections = resizeCanvasAndResults(dimensions, canvas, detections)
+  faceapi.drawDetection(canvas, resizedDetections)
+}
+
+function drawLandmarks(dimensions, canvas, results, withBoxes = true) {
+  const resizedResults = resizeCanvasAndResults(dimensions, canvas, results)
+
+  if (withBoxes) {
+    faceapi.drawDetection(canvas, resizedResults.map(det => det.detection))
+  }
+
+  const faceLandmarks = resizedResults.map(det => det.landmarks)
+  const drawLandmarksOptions = {
+    lineWidth: 2,
+    drawLines: true,
+    color: 'green'
+  }
+  faceapi.drawLandmarks(canvas, faceLandmarks, drawLandmarksOptions)
+}
+
+function drawExpressions(dimensions, canvas, results, withBoxes = true, emotionCallaback) {
+  console.log("eeee" + emotionCallaback)
+  const resizedResults = resizeCanvasAndResults(dimensions, canvas, results)
+
+  if (false && withBoxes) {
+    faceapi.drawDetection(canvas, resizedResults.map(det => det.detection), { withScore: false })
+  }
+
+  faceapi.drawFaceExpressions(canvas,
+      resizedResults.map(({ detection, expressions }) => ({ position: detection.box, expressions })), emotionCallaback)
+}
